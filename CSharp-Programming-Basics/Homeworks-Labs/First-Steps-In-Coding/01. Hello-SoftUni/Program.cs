dd36919a76bcf57bdde29c58b6e569a4d8c9249b
LuboslavBochev <luboslavbochev@abv.bg>
2021-01-19 20:26:07 +0200

    Added First-Steps-In-Coding

---
+++ CSharp-Programming-Basics/Homeworks-Labs/First-Steps-In-Coding/01. Hello-SoftUni/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace First_Steps_In_Coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
