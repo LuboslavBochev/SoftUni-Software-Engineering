5cf89677fec8449910032bf4d25f9010e472bd15
LuboslavBochev <luboslavbochev@abv.bg>
2021-01-20 00:51:55 +0200

    Added Conditional Statements

---
+++ CSharp-Programming-Basics/Homeworks-Labs/Conditional-Statements/07. Area of Figures/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _07._Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = Console.ReadLine();
+            double area = 0;
+
+            if (choice == "square")
+            {
+                double lenght = double.Parse(Console.ReadLine());
+                area = lenght * lenght;
+                Console.WriteLine($"{area:f3}");
+            }
+            else if (choice == "rectangle")
+            {
+                double lenght = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                area = lenght * width;
+                Console.WriteLine($"{area:f3}");
+            }
+            else if (choice == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = (radius * radius) * Math.PI;
+                Console.WriteLine($"{area:f3}");
+            }
+            else if (choice == "triangle")
+            {
+                double lenght = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                area = lenght * (height / 2);
+                Console.WriteLine($"{area:f3}");
+            }
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Homeworks-Labs/Conditional-Statements/08. Toy Shop/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _08._Toy_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input variables
+            double holidayprice = double.Parse(Console.ReadLine());
+            int puzzlesbr = int.Parse(Console.ReadLine());
+            int dollsbr = int.Parse(Console.ReadLine());
+            int bearsbr = int.Parse(Console.ReadLine());
+            int minionsbr = int.Parse(Console.ReadLine());
+            int trucksbr = int.Parse(Console.ReadLine());
+
+
+            double sum = (puzzlesbr * 2.6) + (dollsbr * 3) + (bearsbr * 4.10) + (minionsbr * 8.20) + (trucksbr * 2);
+            int brtoys = puzzlesbr + dollsbr + bearsbr + minionsbr + trucksbr;
+
+            if (brtoys >= 50)
+            {
+                double afterdiscount = sum - (sum * 0.25);
+                double result = afterdiscount - (afterdiscount * 0.10);
+
+                if (result >= holidayprice)
+                {
+                    double holyday = result - holidayprice;
+                    Console.WriteLine($"Yes! {Math.Abs(holyday):f2} lv left.");
+                }
+                else
+                {
+                    double holyday = result - holidayprice;
+                    Console.WriteLine($"Not enough money! {Math.Abs(holyday):f2} lv needed.");
+                }
+            }
+            else if (brtoys < 50)
+            {
+                double amount = sum - (sum * 0.10);
+                if (amount >= holidayprice)
+                {
+                    double holyday = amount - holidayprice;
+                    Console.WriteLine($"Yes! {Math.Abs(holyday):f2} lv left.");
+                }
+
+                else
+                {
+                    double holyday = amount - holidayprice;
+                    Console.WriteLine($"Not enough money! {Math.Abs(holyday):f2} lv needed.");
+                }
+
+            }
+        }
+    }
+}
