df5cf183e3dd5bcda656e36cd6412a7c0669f400
LuboslavBochev <luboslavbochev@abv.bg>
2021-01-19 20:52:02 +0200

    test

---
+++ CSharp-Programming-Basics/Homeworks-Labs/Simple Operations and Calculations/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Simple_Operations_and_Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
