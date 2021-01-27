00235704bba47494b9ee44c8c87fe742131f9d71
LuboslavBochev <luboslavbochev@abv.bg>
2021-01-27 11:30:30 +0200

    Added Exam - 6 and 7 April 2019/01

---
+++ CSharp-Programming-Basics/Exams/Exam - 6 and 7 April 2019/04. Cinema Voucher/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace examTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int coupon = int.Parse(Console.ReadLine()), buyMovie = 0, buyProduct = 0;
+
+            string product = Console.ReadLine();
+
+
+            while (product != "End")
+            {
+                int lenght = product.Length;
+
+                if (lenght > 8)
+                {
+                    char letter1 = char.Parse(product[0].ToString());
+                    char letter2 = char.Parse(product[1].ToString());
+
+                    int sumTwoLetters = (int)letter1 + (int)letter2;
+
+                    if (sumTwoLetters <= coupon)
+                    {
+                        buyMovie++;
+                        coupon -= sumTwoLetters;
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                else if (lenght <= 8)
+                {
+                    char letter1 = char.Parse(product[0].ToString());
+                    int sumOneLetter = (int)letter1;
+
+                    if (sumOneLetter <= coupon)
+                    {
+                        buyProduct++;
+                        coupon -= sumOneLetter;
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                product = Console.ReadLine();
+            }
+            Console.WriteLine($"{buyMovie}");
+            Console.WriteLine($"{buyProduct}");
+        }
+    }
+}
