30c2b717a85f9d277bb6e66afe185057eb4c7e6c
LuboslavBochev <luboslavbochev@abv.bg>
2021-08-10 15:38:13 +0300

    Adding Data-Types-More-Exercise

---
+++ CSharp-Fundamentals/Homeworks-Labs/Data-Types-And-Variables-More-Exercise/03FloatingEquality/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace examTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double eps = 0.000001;
+            double dif = 0;
+            bool isEqual = false;
+
+            if (a > b)
+            {
+                dif = a - b;
+            }
+            else dif = b - a;
+
+            if (dif < eps)
+            {
+                isEqual = true;
+            }
+
+
+            Console.WriteLine(isEqual);
+        }
+    }
+}
