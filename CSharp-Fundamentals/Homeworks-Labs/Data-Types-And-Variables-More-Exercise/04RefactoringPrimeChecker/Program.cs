8921352fd364751e9c0407911332c744e82269d0
LuboslavBochev <luboslavbochev@abv.bg>
2021-08-10 15:33:27 +0300

    Adding Data-Types-And-Variables-More-Exercise

---
+++ CSharp-Fundamentals/Homeworks-Labs/Data-Types-And-Variables-More-Exercise/04RefactoringPrimeChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace examTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int range = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= range; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine("{0} -> true", i);
+                }
+                else Console.WriteLine("{0} -> false", i);
+
+            }
+        }
+    }
+}
---
+++ CSharp-Fundamentals/Homeworks-Labs/Data-Types-And-Variables-More-Exercise/05DecryptingMessage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace examTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int letters = int.Parse(Console.ReadLine());
+            string decrypt = string.Empty;
+
+            for (int i = 1; i <= letters; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                letter += (char)key;
+
+                decrypt += letter;
+            }
+            Console.WriteLine(decrypt);
+        }
+    }
+}
