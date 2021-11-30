6b6fae39059e75ef8db3683f95af4f418bdc0775
Luboslav <luboslavbochev@abv.bg>
2021-11-30 10:58:45 +0200

    Added Basic-Algorithm-Exercise

---
+++ CSharp-Advanced/Homeworks-Labs/Basic-Algorithms-Exercise/01RecursiveArraySum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Alghorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Console.WriteLine(ArraySum(numbers, 0));
+        }
+
+        public static int ArraySum(int[] numbers, int index)
+        {
+            if (index >= numbers.Length)
+            {
+                return 0;
+            }
+            return numbers[index] + ArraySum(numbers, ++index);
+        }
+    }
+}
---
+++ CSharp-Advanced/Homeworks-Labs/Basic-Algorithms-Exercise/02RecursiveFactorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Alghorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(num));
+        }
+
+        public static long Factorial(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+
+            long result = num * Factorial(num - 1);
+            return result;
+        }
+    }
+}
