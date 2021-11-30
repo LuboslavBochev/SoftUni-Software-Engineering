87d7f6ff127493c2083cb925ff4b38d7c142e019
Luboslav <luboslavbochev@abv.bg>
2021-11-30 10:06:18 +0200

    Added Iterators-And-Comparators-Lab

---
+++ CSharp-Advanced/Homeworks-Labs/Iterators-And-Comparators-Lab/03ComparableBook/Library.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+            this.books.Sort();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int index;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public Book Current
+            {
+                get
+                {
+                    return books[index];
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    return this.Current;
+                }
+            }
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                this.index++;
+
+                if (this.index < this.books.Count)
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+            public void Reset() => this.index = -1;
+        }
+    }
+}
