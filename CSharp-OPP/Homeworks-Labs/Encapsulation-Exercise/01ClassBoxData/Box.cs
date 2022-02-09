7978adb74f82876147e2135a754e4d275c0147b5
Luboslav <luboslavbochev@abv.bg>
2022-02-09 13:16:45 +0200

    Adding Encapsulation-Exercise tasks

---
+++ CSharp-OPP/Homeworks-Labs/Encapsulation-Exercise/01ClassBoxData/Box.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        private double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.length = value;
+                }
+                else throw new ArgumentException("Length cannot be zero or negative.");
+            }
+        }
+
+        private double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.width = value;
+                }
+                else throw new ArgumentException("Width cannot be zero or negative.");
+            }
+        }
+
+        private double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.height = value;
+                }
+                else throw new ArgumentException("Height cannot be zero or negative.");
+            }
+        }
+
+        public double SurfaceArea(double length, double width, double height)
+        {
+            return (2 * this.length * this.Width) + (2 * this.length * this.height) + (2 * this.width * this.height);
+        }
+
+        public double LateralSurfaceArea(double length, double width, double height)
+        {
+            return (2 * this.length * this.height) + (2 * this.width * this.height);
+        }
+
+        public double Volume(double length, double width, double height)
+        {
+            return this.length * this.width * this.height;
+        }
+    }
+}
